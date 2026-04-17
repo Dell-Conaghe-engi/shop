0aac1afbac85d9255298dec4ed25115b20648aba
Dell-Conaghe-engi <huj868442@gmail.com>
2026-04-17 19:53:19 +0300

    закончил 7 хандлер

--- bot_utils/message_utils.py
+++ bot_utils/message_utils.py
@@ -3,7 +3,7 @@ def text_for_caption(name, description, base_price):
     text=(
         f"<b>{name}</b>\n"
         f"<b>Описание:</b> {description}\n"
-        f"Цена: {base_price:.2F}/шт"
+        f"Цена: {base_price:.2F}₽"
 
     )
     return text
