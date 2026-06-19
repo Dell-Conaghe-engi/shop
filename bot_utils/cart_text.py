1ae1c4a30b9532567edbe11b7791ac9b3ad3aee0
Dell-Conaghe-engi <huj868442@gmail.com>
2026-06-19 20:15:15 +0300

    фукция генерации текста о содержимом корзины

--- bot_utils/cart_text.py
+++ bot_utils/cart_text.py
@@ -1,2 +1,17 @@
 def generate_cart_text(cart_items):
-    pass
+    '''генерация текста о содержимом корзины'''
+    if not cart_items:
+        return "Ваша корзина пуста"
+
+    text = 'содержимое корзины:\n'
+    total = 0.0
+
+    for item in cart_items:
+        name = item.get('product_name', 'неизвестный товар')
+        quantity = item.get('quantity', 0)
+        final_price = item.get('final_price', 0)
+
+        total = float(final_price)
+        text += f"{name}-{quantity} шт. - по {final_price}₽\n"
+    text += f"Итого: {total}₽"
+    return text
