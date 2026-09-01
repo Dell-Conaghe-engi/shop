c56b0415b80123d2865c3b677999f89bd506a9a0
Dell-Conaghe-engi <huj868442@gmail.com>
2026-09-01 16:44:54 +0300

    оказывается за всё лето я забыл сделать комит так что вот он

--- bot_utils/cart_text.py
+++ bot_utils/cart_text.py
@@ -1,17 +1,20 @@
-def generate_cart_text(cart_items):
-    '''генерация текста о содержимом корзины'''
-    if not cart_items:
-        return "Ваша корзина пуста"
+def generate_cart_text(cart_items: list) -> str:
 
-    text = 'содержимое корзины:\n'
+    if not cart_items:
+        return " Ваша корзина пуста."
+
+    text = " Содержимое корзины:\n\n"
     total = 0.0
 
     for item in cart_items:
-        name = item.get('product_name', 'неизвестный товар')
-        quantity = item.get('quantity', 0)
-        final_price = item.get('final_price', 0)
+        subtotal = float(item["final_price"])
+        total += subtotal
 
-        total = float(final_price)
-        text += f"{name}-{quantity} шт. - по {final_price}₽\n"
-    text += f"Итого: {total}₽"
+        text += (
+            f"{item['product_name']} — "
+            f"{item['quantity']} шт. — "
+            f"{subtotal:.2f} руб\n"
+        )
+
+    text += f"\n Итого: {total:.2f} руб"
     return text
